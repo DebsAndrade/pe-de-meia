cbfbb466a12825931c773b4a3125ea7a0d543136
Débora Andrade <deboraellenandrade@gmail.com>
2026-04-07 18:30:38 +0000

    feat: Refatora a estrutura e o layout dos componentes do aplicativo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,47 @@
-function App() {
+import { useState } from 'react'
+import logo from './assets/logo.png'
+import AddTransaction from './components/AddTransaction'
+import Summary from './components/Summary'
+import TransactionList from './components/TransactionList'
+import { initialTransactions } from './mockData'
+
+export default function App() {
+  const [transactions, setTransactions] = useState(initialTransactions);
+
+  const income = transactions
+    .filter((t) => t.type === 'income')
+    .reduce((acc, t) => acc + t.amount, 0);
+
+  const expense = transactions
+    .filter((t) => t.type === 'expense')
+    .reduce((acc, t) => acc + t.amount, 0);
+
+  const total = income - expense;
+
+  const handleAdd = (newTransaction) => {
+    setTransactions([newTransaction, ...transactions]);
+  };
+
+  const handleDelete = (id) => {
+    setTransactions(transactions.filter((t) => t.id !== id));
+  };
 
   return (
-    <div className="min-h-screen bg-linear-to-r from-blue-500 to-purple-600 flex items-center justify-center">
-      <h1 className="text-3xl font-bold underline text-center text-white">
-        Pé de Meia - O aplicativo que ajuda você a economizar para o futuro!
-      </h1>
+    <div className="mx-auto p-4 bg-white rounded-2xl shadow-sm border border-gray-100">
+      <div className="mx-auto bg-white rounded-2xl shadow-sm p-8 border border-gray-100">
+        <header className="text-center mb-10">
+          <div className="flex items-center justify-center gap-3 mb-2">
+            <img className='w-40 h-38' src={logo} alt="Pé de Meia" />
+          </div>
+          <h1 className="text-slate-500 text-xl">O aplicativo que ajuda você a economizar para o futuro!</h1>
+        </header>
+      </div>
+      <br />
+      <Summary totalBalance={total} income={income} expense={expense} />
+      <br />
+      <AddTransaction onAdd={handleAdd} />
+      <br />
+      <TransactionList transactions={transactions} onDelete={handleDelete} />
     </div>
   )
-}
-
-export default App
+};
