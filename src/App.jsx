f4470e7b496eb659b7525712e1fc348130522e37
Débora Andrade <deboraellenandrade@gmail.com>
2026-04-14 17:25:34 +0000

    feat: adiciona gerenciamento de tema no componente App com uso do ThemeContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
+import { useContext } from 'react'
 import AddTransaction from './pages/AddTransaction'
 import { Routes, Route } from 'react-router'
 import MainLayout from './layouts/MainLayout'
 import Dashboard from './pages/Dashboard'
 import History from './pages/History'
 import Settings from './pages/Settings'
+import { ThemeContext } from './context/ThemeContext'
 
 export default function App() {
+  const { theme } = useContext(ThemeContext)
 
   return (
-    <div className="mx-auto p-4 bg-white rounded-2xl shadow-sm border border-gray-100">
+    <div
+      className={`app ${theme} mx-auto p-4 shadow-sm border ${theme === 'light' ? 'bg-white border-gray-100 text-gray-900' : 'bg-gray-900 border-gray-700 text-gray-100'}`}
+    >
       <Routes>
         <Route path='/' element={<MainLayout />}>
           <Route index element={<Dashboard />} />
+          <Route path="transactions" element={<AddTransaction />} />
           <Route path="history" element={<History />} />
           <Route path="settings" element={<Settings />} />
-          <Route path="transactions" element={<AddTransaction />} />
         </Route>
       </Routes>
     </div>
