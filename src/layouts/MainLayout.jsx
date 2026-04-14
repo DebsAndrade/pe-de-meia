bda8e9b2d0736f3ea52f470f61a700b83a0d221e
Débora Andrade <deboraellenandrade@gmail.com>
2026-04-14 17:24:33 +0000

    feat: adiciona suporte a tema claro/escuro no MainLayout com botão de alternância

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,7 +1,11 @@
 import { NavLink, Outlet } from "react-router";
+import { useContext } from 'react';
 import logo from '../assets/logo.png';
+import { ThemeContext } from '../context/ThemeContext';
 
 export default function MainLayout() {
+    const { theme, toggleTheme } = useContext(ThemeContext)
+
     return (
         <div>
             <nav style={{ display: "flex", gap: "12px", marginBottom: "20px" }}>
@@ -30,13 +34,16 @@ export default function MainLayout() {
                 >
                     Transactions
                 </NavLink>
+                <button onClick={toggleTheme}>
+                    {theme === 'dark' ? '☀️' : '🌙'}
+                </button>
             </nav>
-            <div className="mx-auto bg-white rounded-2xl shadow-sm p-8 border border-gray-100">
+            <div className={`mx-auto rounded-2xl shadow-sm p-8 border ${theme === 'light' ? 'bg-white border-gray-100' : 'bg-gray-800 border-gray-700'}`}>
                 <header className="text-center mb-10">
                     <div className="flex items-center justify-center gap-3 mb-2">
                         <img className='w-40 h-38' src={logo} alt="Pé de Meia" />
                     </div>
-                    <h1 className="text-slate-500 text-xl">O aplicativo que ajuda você a economizar para o futuro!</h1>
+                    <h1 className={`text-xl ${theme === 'light' ? 'text-slate-500' : 'text-slate-300'}`}>O aplicativo que ajuda você a economizar para o futuro!</h1>
                 </header>
             </div>
             <Outlet />
