70ae96f387868eef47ec7d06026a7d037cb54c89
Débora Andrade <deboraellenandrade@gmail.com>
2026-04-14 17:25:23 +0000

    feat: reorganiza a estrutura de provedores no componente principal para melhor gerenciamento de contexto

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,22 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ThemeProvider } from './context/ThemeContext'
+import { PreferencesProvider } from './context/PreferencesContext'
 
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <ReactQueryDevtools initialIsOpen={false} />
-        <App />
-      </BrowserRouter>
+      <ThemeProvider>
+        <PreferencesProvider>
+          <BrowserRouter>
+            <ReactQueryDevtools initialIsOpen={false} />
+            <App />
+          </BrowserRouter>
+        </PreferencesProvider>
+      </ThemeProvider>
     </QueryClientProvider>
   </StrictMode>
 )
