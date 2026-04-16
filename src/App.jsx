7247e01ad146e97bd63111cd18eda10cd3a2984a
Débora Andrade <deboraellenandrade@gmail.com>
2026-04-16 18:50:57 +0000

    fix: Atualizar os caminhos das imagens e melhorar a estrutura HTML para maior acessibilidade

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,21 @@
-import { useContext } from 'react'
 import AddTransaction from './pages/AddTransaction'
 import { Routes, Route } from 'react-router'
 import MainLayout from './layouts/MainLayout'
 import Dashboard from './pages/Dashboard'
 import History from './pages/History'
 import Settings from './pages/Settings'
-import { ThemeContext } from './context/ThemeContext'
 
 export default function App() {
-  const { theme } = useContext(ThemeContext)
-
   return (
-    <div
-      className={`app ${theme} mx-auto p-4 shadow-sm border ${theme === 'light' ? 'bg-white border-gray-100 text-gray-900' : 'bg-gray-900 border-gray-700 text-gray-100'}`}
-    >
+    <div className="min-h-screen bg-background text-foreground">
       <Routes>
         <Route path='/' element={<MainLayout />}>
           <Route index element={<Dashboard />} />
-          <Route path="transactions" element={<AddTransaction />} />
-          <Route path="history" element={<History />} />
-          <Route path="settings" element={<Settings />} />
+          <Route path="adicionar" element={<AddTransaction />} />
+          <Route path="historico" element={<History />} />
+          <Route path="definicoes" element={<Settings />} />
         </Route>
       </Routes>
     </div>
   )
-};
+}
